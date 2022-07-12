30b84727e482fd5b80c4c03e75a2dbc4a832dbf4
Nick Ciambrone <nickciambrone@Nicks-MBP.fios-router.home>
2022-07-12 13:51:21 -0400

    stripe integration

--- src/pages/shop/shop.page.jsx
+++ src/pages/shop/shop.page.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import { useState } from 'react';
+import {loadStripe} from '@stripe/stripe-js'
+
+const stripePromise = loadStripe(
+    'pk_live_3AFOaBJhRRYDIeUjNcAKPjZV006D7xNPLb'
+)
 
 const Shop = () =>{
+    const [stripeError, setStripeError]=useState();
+    const [loading, setLoading]=useState();
+
+    const handleClick = async () =>{
+        setLoading(true);
+        const stripe = await stripePromise;
+        const {error} = await stripe.redirectToCheckout({
+            lineItems:[{
+                price:'price_1LKnCXGBGKe5K8ugFRnwwEj8',
+                quantity:1
+            }],
+            mode:'payment',
+            cancelUrl:window.location.origin,
+            successUrl:'https://quiet-chaja-d2e83e.netlify.app/'
+        });
+        if(error){
+            setLoading(false);
+            setStripeError(error);
+        }
+    };
+
     return (
         <div className = 'shop'>
-        Shop
+        {stripeError && <p style={{color:'red'}}>{stripeError}</p>}
+        <button role = 'link' onClick={handleClick} disabled={loading}>
+            Go to checkout
+        </button>
         </div>
     )
 }
