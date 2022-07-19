c73ba04baa4765164c42349bf06f6b4e55676118
Nick Ciambrone <nickciambrone@Nicks-MBP.fios-router.home>
2022-07-19 10:56:11 -0400

    cleaned shop cards

--- src/pages/shop/shop.page.jsx
+++ src/pages/shop/shop.page.jsx
@@ -15,6 +15,7 @@ const Shop = () => {
   const [stripeError, setStripeError] = useState();
   const [loading, setLoading] = useState();
   const [priceId, setPriceId] = useState([]);
+  const [all, setAll] = useState(false);
 
   console.log(priceId);
   const removeFirst = (item, items) => {
@@ -62,82 +63,89 @@ const Shop = () => {
   return (
     <div className="shop">
       <div className="shop-shop">
-        <div className="shop-items">
-          {Object.keys(SHOP_DATA).map((ele) =>
-            SHOP_DATA[ele].map((elem) =>
-              ele === category || category === "all" ? (
-                <div
-                  className="shop-item-container"
-                  style={{ display: "flex", flexDirection: "column" }}
-                >
-                  <img
-                    style={{ width: "100%" }}
-                    src={"/images" + elem.imgUrl}
-                  />
-                  <div className="shop-item-info">
-                    <div className="item-info">
-                      {elem.title} / ${elem.price}
-                    </div>
-                  </div>
-                  {stripeError && <p style={{ color: "red" }}>{stripeError}</p>}
+        <div style={{display:'flex', flexDirection:'column'}}>
+          <div className="shop-items" style={{marginBottom:'20px'}}>
+            {Object.keys(SHOP_DATA).map((ele) =>
+              SHOP_DATA[ele].map((elem, ind) =>
+                (ele === category || category === "all") &&
+                (all === true || ind < 8) ? (
                   <div
-                    style={{
-                      display: "flex",
-                      flexDirection: "row",
-                      justifyContent: "space-evenly",
-                      marginBottom: "5px",
-                      fontSize: "18px",
-                    }}
+                    className="shop-item-container"
+                    style={{ display: "flex", flexDirection: "column" }}
                   >
-                    <div
-                      role="link"
-                      disabled={loading}
-                      className="add-to-cart"
-                      style={{ width: "25%" }}
-                    >
-                      <img
-                        src={minus}
-                        style={{ cursor: "pointer" }}
-                        width='30px'
-                        onClick={() =>
-                          setPriceId(removeFirst(elem.priceId, priceId))
-                        }
-                      />
+                    <img
+                      style={{ width: "100%" }}
+                      src={"/images" + elem.imgUrl}
+                    />
+                    <div className="shop-item-info">
+                      <div className="item-info">
+                        {elem.title} / ${elem.price}
+                      </div>
                     </div>
-                    <span >
-                      {priceId.filter((ele) => ele === elem.priceId).length}
-                    </span>
+                    {stripeError && (
+                      <p style={{ color: "red" }}>{stripeError}</p>
+                    )}
                     <div
-                      role="link"
-                      disabled={loading}
-                      className="add-to-cart"
-                      style={{ width: "25%" }}
+                      style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        justifyContent: "space-evenly",
+                        marginBottom: "5px",
+                        fontSize: "18px",
+                      }}
                     >
-                      <img
-                        src={plus}
-                        width='30px'
-
-                        style={{ cursor: "pointer" }}
-                        onClick={() => {
-                          setPriceId([...priceId, elem.priceId]);
-                        }}
-                      />
+                      <div
+                        role="link"
+                        disabled={loading}
+                        className="add-to-cart"
+                        style={{ width: "25%" }}
+                      >
+                        <img
+                          src={minus}
+                          style={{ cursor: "pointer" }}
+                          width="30px"
+                          onClick={() =>
+                            setPriceId(removeFirst(elem.priceId, priceId))
+                          }
+                        />
+                      </div>
+                      <span>
+                        {priceId.filter((ele) => ele === elem.priceId).length}
+                      </span>
+                      <div
+                        role="link"
+                        disabled={loading}
+                        className="add-to-cart"
+                        style={{ width: "25%" }}
+                      >
+                        <img
+                          src={plus}
+                          width="30px"
+                          style={{ cursor: "pointer" }}
+                          onClick={() => {
+                            setPriceId([...priceId, elem.priceId]);
+                          }}
+                        />
+                      </div>
                     </div>
+                    <button
+                      role="link"
+                      onClick={() => checkout([elem.priceId])}
+                      disabled={loading}
+                      className="buy-now"
+                    >
+                      Buy Now
+                    </button>
                   </div>
-                  <button
-                    role="link"
-                    onClick={() => checkout([elem.priceId])}
-                    disabled={loading}
-                    className="buy-now"
-                  >
-                    Buy Now
-                  </button>
-                </div>
-              ) : (
-                ""
+                ) : (
+                  ""
+                )
               )
-            )
-          )}
+            )}
+          </div>
+          <div className="see-all" onClick={() => setAll(true)}>
+            See All Results
+          </div>
         </div>
         <div className="checkout-side">
           <div
